16667ca88583f859c274b939ab277fe3b96ba4bc
Chiara Masci <studente.chiara.masci@pavoniani.tn.it>
2016-07-06 23:51:26 +0200

    version missing upload to server

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-app = angular.module("spectrometer", ['ionic', 'ngCordova'])
+app = angular.module("spectrometer", ['ionic', 'ngCordova', 'ui.router'])
+index = angular.module("index", ['ionic', 'ngCordova'])
+result = angular.module("result", ['ionic', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -24,9 +26,19 @@ app = angular.module("spectrometer", ['ionic', 'ngCordova'])
 });
 
 
-app.controller('banana', function($scope, $cordovaCamera, $cordovaFile) {
+
+index.controller("ContentController", function($scope, $ionicSideMenuDelegate){
+	$scope.toggleLeft = function() {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+});
+
+var data_sent = 0;
+
+app.controller('banana', function($scope, $cordovaCamera, $cordovaFile, $state) {
     $scope.images = [];
-	var check_photo = 0; 
+	var check_photo = 0;
+	
  
     $scope.addImage = function() {
         alert("add image");
@@ -109,13 +121,10 @@ app.controller('banana', function($scope, $cordovaCamera, $cordovaFile) {
 
 	$scope.upload_file = function(){
 
-	
-
 	if (check_photo == 0){
 		alert('Please, take the photo');}
 
 	else{
-	alert('Photo elaboration...');
 	var canvas = document.getElementById("canvas"),
 		      context = canvas.getContext("2d"),
           photo = document.getElementById("photo");
@@ -125,7 +134,7 @@ app.controller('banana', function($scope, $cordovaCamera, $cordovaFile) {
           var Pic = document.getElementById("canvas").toDataURL("image/png");
             Pic = Pic.replace(/^data:image\/(png|jpg);base64,/, "");
 
-	alert('Upload start');
+	alert('Upload started. Touch "ok" to continue');
 
             $.ajax({
                 url: '/upload_img',
@@ -134,7 +143,10 @@ app.controller('banana', function($scope, $cordovaCamera, $cordovaFile) {
                 contentType: 'application/json; charset=utf-8',
                 dataType: 'json',
                 success: function (msg) {
-                    alert("Done, Picture Uploaded. See the result :)");
+                    alert("Done, Picture Uploaded. See the result tapping on the tick");
+					document.getElementById('tick').style.opacity="1";
+					document.getElementById('circle').style.opacity="0.3";
+					data_sent = 1;
                 },
                 error: function (jqXHR, status, err) {
                     alert(err.message);
@@ -150,235 +162,20 @@ app.controller('banana', function($scope, $cordovaCamera, $cordovaFile) {
 });
 
 
+result.controller("results_controller", function($scope){
 
-/*app.controller('banana', function($scope, $cordovaCamera, $cordovaFile) {
-    $scope.images = [];
-	var check_photo = 0; 
- 
-    $scope.addImage = function() {
-        alert("add image");
-		var check_photo = true:
+	var output = document.getElementById('output');
+	var camera = document.getElementById('camera');
 
-			var options = {
-			targetWidth: 250,
-	  		targetHeight: 250,
-			destinationType : Camera.DestinationType.FILE_URI,
-			sourceType : Camera.PictureSourceType.CAMERA, // Camera.PictureSourceType.PHOTOLIBRARY
-			allowEdit : true,
-			encodingType: Camera.EncodingType.JPEG,
-			popoverOptions: CameraPopoverOptions,
-			};
-	
-		// 3
-		$cordovaCamera.getPicture(options).then(function(imageData) {
- 
-		// 4
-		onImageSuccess(imageData);
- 
-		function onImageSuccess(fileURI) {
-			createFileEntry(fileURI);
-		}
- 
-		function createFileEntry(fileURI) {
-			window.resolveLocalFileSystemURL(fileURI, copyFile, fail);
-		}
- 
-		// 5
-		function copyFile(fileEntry) {
-			var name = fileEntry.fullPath.substr(fileEntry.fullPath.lastIndexOf('/') + 1);
-			var newName = makeid() + name;
- 
-			window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function(fileSystem2) {
-				fileEntry.copyTo(
-					fileSystem2,
-					newName,
-					onCopySuccess,
-					fail
-				);
-			},
-			fail);
-		return newName;
-		}
-		
-		// 6
-		function onCopySuccess(entry) {
-			$scope.$apply(function () {
-				$scope.images.push(entry.nativeURL);
-			});
-		}
- 
-		function fail(error) {
-			console.log("fail: " + error.code);
-		}
- 
-		function makeid() {
-			var text = "";
-			var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
- 
-			for (var i=0; i < 5; i++) {
-				text += possible.charAt(Math.floor(Math.random() * possible.length));
-			}
-			return text;
-		}
- 
-	}, function(err) {
-		console.log(err);
+	if (data_sent == 0){
+		alert('You should take the picture to have the results');
+		output.innerHTML = "unknown";
+		camera.style.display = "none";}
+	else{
+		output.innerHTML = "variable has to be inserted here";}
+
 	});
 
-		}
-	
-	$scope.urlForImage = function(imageName) {
-  var name = imageName.substr(imageName.lastIndexOf('/') + 1);
-  var trueOrigin = cordova.file.dataDirectory + name;
-  return trueOrigin;}
-  	
-
-
-	$scope.upload_file = function(){
-
-	alert('start drawing on canvas');
 
 
 
-	var canvas = document.getElementById("canvas"),
-		      context = canvas.getContext("2d"),
-          photo = document.getElementById("photo");
-
-          context.drawImage(photo, 0, 0, 300, 300);
-
-          var Pic = document.getElementById("canvas").toDataURL("image/png");
-            Pic = Pic.replace(/^data:image\/(png|jpg);base64,/, "");
-
-	alert('Start ajax');
-
-            $.ajax({
-                url: '/upload_img',
-                type: 'POST',
-                data: '{ "imageData" : "' + Pic + '" , "id_db" : ' + $('#id_db').val() + '}',
-                contentType: 'application/json; charset=utf-8',
-                dataType: 'json',
-                success: function (msg) {
-                    alert("Done, Picture Uploaded. See the result.");
-                },
-                error: function (jqXHR, status, err) {
-                    alert(err.message);
-                    alert("Oh no, something went wrong :(");
-                }
-            });	
-
- 		}
-
-});
-*/
-
-
-
-/*app.controller('banana', function($scope, $cordovaCamera, $cordovaFile) {
-    $scope.images = [];
-	i = 0;
- 
-    $scope.addImage = function() {
-        alert("add image");
-
-			var options = {
-			targetWidth: 250,
-	  		targetHeight: 250,
-			destinationType : Camera.DestinationType.FILE_URI,
-			sourceType : Camera.PictureSourceType.CAMERA, // Camera.PictureSourceType.PHOTOLIBRARY
-			allowEdit : true,
-			encodingType: Camera.EncodingType.JPEG,
-			popoverOptions: CameraPopoverOptions,
-			};
-	
-		// 3
-		$cordovaCamera.getPicture(options).then(function(imageData) {
- 
-		// 4
-		onImageSuccess(imageData);
- 
-		function onImageSuccess(fileURI) {
-			createFileEntry(fileURI);
-		}
- 
-		function createFileEntry(fileURI) {
-			window.resolveLocalFileSystemURL(fileURI, copyFile, fail);
-		}
- 
-		// 5
-		function copyFile(fileEntry) {
-			var name = fileEntry.fullPath.substr(fileEntry.fullPath.lastIndexOf('/') + 1);
-			var newName = makeid() + name;
- 
-			window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function(fileSystem2) {
-				fileEntry.copyTo(
-					fileSystem2,
-					newName,
-					onCopySuccess,
-					fail
-				);
-			},
-			fail);
-		return newName;
-		}
-		
-		// 6
-		function onCopySuccess(entry) {
-			$scope.$apply(function () {
-				$scope.images.push(entry.nativeURL);
-			});
-		}
- 
-		function fail(error) {
-			console.log("fail: " + error.code);
-		}
- 
-		function makeid() {
-			var text = "";
-			var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
- 
-			for (var i=0; i < 5; i++) {
-				text += possible.charAt(Math.floor(Math.random() * possible.length));
-			}
-			return text;
-		}
- 
-	}, function(err) {
-		console.log(err);
-	});
-
-		}
-	
-	$scope.urlForImage = function(imageName) {
-  var name = imageName.substr(imageName.lastIndexOf('/') + 1);
-  var trueOrigin = cordova.file.dataDirectory + name;
-  return trueOrigin;}
-  	
-
-
-	$scope.upload_file = function(){
-
-	alert('start uploading');
-
-	var Pic = document.getElementById("photo");
-	
-            Pic = Pic.replace(/^data:image\/(png|jpg);base64,/, "");
-			alert('buru');
-
-            $.ajax({
-                url: '/upload_img',
-                type: 'POST',
-                data: '{ "imageData" : "' + Pic + '" , "id_db" : ' + $('#id_db').val() + '}',
-                contentType: 'application/json; charset=utf-8',
-                dataType: 'json',
-                success: function (msg) {
-                    //alert("Done, Picture Uploaded.");
-                    alert('Uploaded successfully');
-                },
-                error: function (jqXHR, status, err) {
-                    alert(err.message)
-                    alert("Local error callback.");
-                }
-            });}	
- 		
-
-});*/
